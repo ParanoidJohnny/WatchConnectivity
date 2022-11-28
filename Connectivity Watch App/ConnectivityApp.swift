81dbac860bc1aa06df24b1082e5e5975d057f8c1
ParanoidJohnny <ahmedgazez@gmail.com>
2022-11-28 01:27:58 +0100

    Initial Commit

---
+++ Connectivity Watch App/ConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectivityApp.swift
+//  Connectivity Watch App
+//
+//  Created by AhmedFatma on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Connectivity_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connectivity/ConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectivityApp.swift
+//  Connectivity
+//
+//  Created by AhmedFatma on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
